e33d6529de8089ede5cfb6d88adb0f4751b6d186
itslu666 <philipp.j6y@gmail.com>
2024-04-15 20:40:51 +0200

    fixed icon

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ctk_color_picker_widget as CTkColorPicker
 
 root = ctk.CTk()
 root.title("ColorPicker")
-root.iconbitmap("C:/Users/Philipp/Downloads/pngwing.com.ico")
+root.iconbitmap("pngwing.com.ico")
 
 cp = CTkColorPicker.CTkColorPicker(root, orientation="horizontal")
 cp.pack(expand=True, fill="both")
