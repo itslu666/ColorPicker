4d8b30fe00f3324ece6c1df6ddf19c962cb10632
itslu666 <philipp.j6y@gmail.com>
2024-04-15 20:21:47 +0200

    fixed issues

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import customtkinter as ctk
 import ctk_color_picker_widget as CTkColorPicker
 
 root = ctk.CTk()
+root.title("ColorPicker")
+root.iconbitmap("C:/Users/Philipp/Downloads/pngwing.com.ico")
 
-cp = CTkColorPicker.CTkColorPicker(root)
+cp = CTkColorPicker.CTkColorPicker(root, orientation="horizontal")
 cp.pack(expand=True, fill="both")
 
 
